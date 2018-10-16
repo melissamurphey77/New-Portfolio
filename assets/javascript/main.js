3fee5e0f34c6302b9127be2c926506dffed941d0
Melissa Murphey <melissamurphey77@gmail.com>
2018-10-16 17:45:39 -0500

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,15 @@
 .navbar {
     margin - bottom: 0;
     border - radius: 0;
-    background-color: #750D37
+    background-color: #750D37;
 }
 
 
 .footer {
-    background-color: #750D37;
+    background-color: #darkpurple;
     padding: 25px;
+}
+
+.motto {
+    font
 }
